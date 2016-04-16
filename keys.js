918f3cfb061c0d49ac7edff73605805786c663dc
johnnyluck87 <johnserencses@gmail.com>
2016-04-16 08:16:47 -0400

    updated twitter keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'A0gKyCuLSSttxcpbXh4y2egwQ',
+  consumer_secret: 'ekqtqllaCcrwTUkWd77XBU7zf5PgLbIhbvqgOK94aWqs5N8QzP',
+  access_token_key: '721301681800155137-KkNzgI93o79bSsXTJiRikgrVfGytVHQ',
+  access_token_secret: '2jOnqWA0lBUpzdZ18x38pY1vw7dXoZZuiRZf6U8RDAfxZ',
 }
